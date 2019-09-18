252719b98111073c2c26ed41485d543456a6f340
Nicole K <nicolekleimanyedid@gmail.com>
2019-09-18 10:29:02 -0500

    Ya sirveee

--- ibero-dci3-galeria-iconos-master/script.js
+++ ibero-dci3-galeria-iconos-master/script.js
@@ -6,8 +6,53 @@ function mostrarIconoPrincipal(icono) {
   mainIcon.classList.add(icono);
 }
 
-function eliminarIcono() {
-  event.stopPropagation();
-  iconos.splice(index, 1);
-  imprimirToodsLosIconos();
+function nuevoIcono(){
+  nombreicono = document.getElementById("icon_input").value;
+  iconos.push(nombreicono);
+  imprimirTodosLosIconos();
+
 }
+
+function validarNombre(){
+  nombreicono = document.getElementById("icon_input").value;
+
+  if (nombreicono == ""){
+    return false;
+  }else{
+    return true;
+  }
+}
+
+function validarTodo(){
+  var nombre = validarNombre();
+  if(nombre){
+    nuevoIcono();
+  }
+}
+
+function imprimirArreglo(propiedad, index){
+var lista = document.getElementById("icons_list");
+lista.insertAdjacentHTML('beforeend',
+  `<li class="icon_container" onclick="mostrarIconoPrincipal('` + propiedad + `')">
+    <i class="close_icon fas fa-times" onclick="eliminarIcono(` + index + `)"></i>
+    <i class="icon fas ` + propiedad + ` "></i>
+    <div class="label">` + propiedad + `</div>
+  </li>`
+  );
+}
+
+
+function eliminarIcono(index) {
+  iconos.splice(index, 1);
+  imprimirTodosLosIconos();
+}
+
+function imprimirTodosLosIconos(){
+  var lista = document.getElementById("icons_list");
+  lista.innerHTML = "";
+  iconos.forEach(imprimirArreglo);
+
+
+}
+
+imprimirTodosLosIconos();
